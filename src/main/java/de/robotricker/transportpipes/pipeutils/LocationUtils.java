b285050714e7be4e861aae6754458ddcc4f4b78e
Gabriele C <sgdc3.mail@gmail.com>
2017-07-08 02:18:19 +0200

    Use tabs...

--- src/main/java/de/robotricker/transportpipes/pipeutils/LocationUtils.java
+++ src/main/java/de/robotricker/transportpipes/pipeutils/LocationUtils.java
@@ -8,19 +8,19 @@ import java.util.List;
 
 public class LocationUtils {
 
-    private LocationUtils() {
-    }
+	private LocationUtils() {
+	}
 
-    public static List<Block> getNearbyBlocks(Location location, int radius) {
-        List<Block> blocks = new ArrayList<>();
-        Block middle = location.getBlock();
-        for (int x = radius; x >= -radius; x--) {
-            for (int y = radius; y >= -radius; y--) {
-                for (int z = radius; z >= -radius; z--) {
-                    blocks.add(middle.getRelative(x, y, z));
-                }
-            }
-        }
-        return blocks;
-    }
+	public static List<Block> getNearbyBlocks(Location location, int radius) {
+		List<Block> blocks = new ArrayList<>();
+		Block middle = location.getBlock();
+		for (int x = radius; x >= -radius; x--) {
+			for (int y = radius; y >= -radius; y--) {
+				for (int z = radius; z >= -radius; z--) {
+					blocks.add(middle.getRelative(x, y, z));
+				}
+			}
+		}
+		return blocks;
+	}
 }
