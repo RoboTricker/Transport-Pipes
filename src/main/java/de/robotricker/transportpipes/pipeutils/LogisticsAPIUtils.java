36a1c1c7947a86744aa61090ce7d6eeef3a228a4
Simon Rost <rost.simon@frontfight.net>
2017-08-15 19:41:35 +0200

    guaranteed compatibility between TransportPipes and LogisticsAPI interfaces

--- src/main/java/de/robotricker/transportpipes/pipeutils/LogisticsAPIUtils.java
+++ src/main/java/de/robotricker/transportpipes/pipeutils/LogisticsAPIUtils.java
@@ -1,9 +1,53 @@
 package de.robotricker.transportpipes.pipeutils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import com.logisticscraft.logisticsapi.BlockSide;
+import com.logisticscraft.logisticsapi.event.ItemContainerRegisterEvent;
+import com.logisticscraft.logisticsapi.event.ItemContainerUnregisterEvent;
+
+import de.robotricker.transportpipes.api.PipeAPI;
+import de.robotricker.transportpipes.api.TransportPipesContainer;
+import de.robotricker.transportpipes.pipeitems.ItemData;
+import de.robotricker.transportpipes.pipes.FilteringMode;
+import de.robotricker.transportpipes.pipes.PipeDirection;
 
 public class LogisticsAPIUtils implements Listener {
 
-	
-	
+	@EventHandler
+	public void onRegister(final ItemContainerRegisterEvent e) {
+		PipeAPI.unregisterTransportPipesContainer(e.getLocation());
+		PipeAPI.registerTransportPipesContainer(e.getLocation(), new TransportPipesContainer() {
+
+			@Override
+			public ItemStack insertItem(PipeDirection insertDirection, ItemStack insertion) {
+				return e.getItemContainer().insertItem(BlockSide.fromBlockFace(insertDirection.toBlockFace()), insertion);
+			}
+
+			@Override
+			public int howMuchSpaceForItemAsync(PipeDirection insertDirection, ItemStack insertion) {
+				return e.getItemContainer().howMuchSpaceForItemAsync(BlockSide.fromBlockFace(insertDirection.toBlockFace()), insertion);
+			}
+
+			@Override
+			public ItemStack extractItem(PipeDirection extractDirection, int extractAmount, List<ItemData> filterItems, FilteringMode filteringMode) {
+				List<ItemStack> wrappedFilterItems = new ArrayList<>();
+				for (ItemData id : filterItems) {
+					wrappedFilterItems.add(id.toItemStack());
+				}
+				return e.getItemContainer().extractItem(BlockSide.fromBlockFace(extractDirection.toBlockFace()), extractAmount, wrappedFilterItems, com.logisticscraft.logisticsapi.item.FilteringMode.fromId(filteringMode.getId()));
+			}
+		});
+	}
+
+	@EventHandler
+	public void onUnregister(ItemContainerUnregisterEvent e) {
+		PipeAPI.unregisterTransportPipesContainer(e.getLocation());
+	}
+
 }
