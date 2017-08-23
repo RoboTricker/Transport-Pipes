932c7ba7c4babd401aaea064f8294d9c57acfe87
Gabriele C <sgdc3.mail@gmail.com>
2017-08-23 19:14:56 +0200

    Add pipe crafting permissions
    Suggestion: create a new type for the white pipe

--- src/main/java/de/robotricker/transportpipes/pipeutils/CraftUtils.java
+++ src/main/java/de/robotricker/transportpipes/pipeutils/CraftUtils.java
@@ -3,6 +3,7 @@ package de.robotricker.transportpipes.pipeutils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
@@ -33,24 +34,49 @@ public class CraftUtils implements Listener {
 
 	@EventHandler
 	public void onPrepareCraft(PrepareItemCraftEvent e) {
+		// Prevent more viewers
+		if(e.getViewers().size() != 1) {
+			return;
+		}
+		HumanEntity viewer = (HumanEntity) e.getViewers().toArray()[0];
 
+		// Check if a recipe is being used
 		Recipe r = e.getInventory().getRecipe();
 		if (r == null) {
 			return;
 		}
 
-		//prevent colored pipe crafting if the given pipe is not a colored pipe
-		if (PipeType.getFromPipeItem(r.getResult()) != null) {
-			boolean prevent = false;
+		PipeType resultType = PipeType.getFromPipeItem(r.getResult());
+		PipeColor resultColor = PipeColor.getPipeColorByPipeItem(r.getResult());
+
+		// If no pipe is being crafted ignore the event
+		if(resultType == null) {
+			return;
+		}
+
+		boolean prevent = false;
+
+		// Check normal pipe permission
+		if(resultColor != null && resultColor.equals(PipeColor.WHITE) && !viewer.hasPermission(TransportPipes.instance.generalConf.getPermissionCraftPipe())) {
+			prevent = true;
+		}
+		// Check other pipe permission
+		else if(!viewer.hasPermission(resultType.getCraftingPermission())) {
+			prevent = true;
+		}
+		// Prevent colored pipe crafting if the given pipe is not a colored pipe
+		else {
 			for (int i = 1; i < 10; i++) {
 				ItemStack is = e.getInventory().getItem(i);
 				if (is != null && is.getType() == Material.SKULL_ITEM && is.getDurability() == SkullType.PLAYER.ordinal()) {
 					prevent |= PipeType.getFromPipeItem(is) == null;
 				}
 			}
-			if (prevent) {
-				e.getInventory().setResult(null);
-			}
+		}
+
+		// Cancel result if required
+		if (prevent) {
+			e.getInventory().setResult(null);
 		}
 	}
 
