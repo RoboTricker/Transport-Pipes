5e9caf140e9829a7f03746a6afcafaa01a50bad4
Simon Rost <rost.simon@frontfight.net>
2018-09-18 22:30:47 +0200

    #BUGFIX 'priority:design' 'functionality:basicDuctType'
    {removed basicDuctType from Duct class}

--- src/main/java/de/robotricker/transportpipes/ducts/pipe/Pipe.java
+++ src/main/java/de/robotricker/transportpipes/ducts/pipe/Pipe.java
@@ -3,7 +3,6 @@ package de.robotricker.transportpipes.ducts.pipe;
 import org.bukkit.Chunk;
 
 import de.robotricker.transportpipes.ducts.Duct;
-import de.robotricker.transportpipes.ducts.types.BasicDuctType;
 import de.robotricker.transportpipes.ducts.types.DuctType;
 import de.robotricker.transportpipes.ducts.types.PipeType;
 import de.robotricker.transportpipes.utils.BlockLoc;
@@ -11,7 +10,7 @@ import de.robotricker.transportpipes.utils.BlockLoc;
 public class Pipe extends Duct {
 
     public Pipe(BlockLoc blockLoc, Chunk chunk, DuctType pipeType) {
-        super(BasicDuctType.valueOf("Pipe"), pipeType, blockLoc, chunk);
+        super(pipeType, blockLoc, chunk);
     }
 
     @Override
